d6421a21e5bc292b7f10d299377ff0be4d15d714
Jose Burgardt <joseburg89@hotmail.com>
2012-03-14 10:53:47 -0300

    3er commit

--- Script.js
+++ Script.js
@@ -11,11 +11,20 @@ $("#buttonday1").click(function(event) {
         
         var param = $("#alias").val();
 	url = "http://localhost/day1/api/dispatcher.php";
-        
+    
+	// Para el punto 7 pongo mal la url
+	// url = "http://localhost/day/api/dispatcher.php";
+    
         $.post(url, {service: 'welcome.hello', params: {name: param}},
 		function(data){
                     //alert(data);
                     $("#response").html(data);
 		});
 
+});
+
+$("#msg").ajaxError(function(event, request, settings){
+  $("#response").hide();  
+  $(this).append("Error requesting page " + settings.url);
+  $("#msg").css("background","red");
 });
